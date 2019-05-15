fc5942994d94b23be25d62be7c81dc7e2c0e0c4d
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-05-15 10:07:01 -0700

    Updated js and html to have a button that changes the color of the text.

---
+++ js-assignment.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+
+	console.log("Hello World");
+
+
+document.getElementById('changeColorText').addEventListener ('click', function (event) {
+		document.body.style.color = "red";
+		//document.getElementById("changeColorText").src = "./js-assignment.js";
+	}
+)};
+
