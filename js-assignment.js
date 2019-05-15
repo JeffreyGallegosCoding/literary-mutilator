a31f7f5cd02f45ff4ddeabd7deea84f37e44d460
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-05-15 10:21:40 -0700

    Added a button in html and added a function to change the paragraph text in js.

--- js-assignment.js
+++ js-assignment.js
@@ -3,10 +3,20 @@ window.onload = function() {
 
 	console.log("Hello World");
 
-
+//Change the font color function
 document.getElementById('changeColorText').addEventListener ('click', function (event) {
 		document.body.style.color = "red";
 		//document.getElementById("changeColorText").src = "./js-assignment.js";
 	}
-)};
+);
+
+//Change the text font function
+	document.getElementById('changeTextFont').addEventListener ('click', function (event) {
+			document.body.style.fontFamily = "verdana";
+			//document.getElementById("changeTextFont").src = "./js-assignment.js";
+		}
+	);
+};
+
+
 
