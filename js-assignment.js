5a2b4a94b6cd834e10f8acc38ef21fbe4d5f6a83
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-05-18 15:57:35 -0700

    Changed variables in shuffler and messing with code in js file. Keep getting Reference error for window.onload function.

--- js-assignment.js
+++ js-assignment.js
@@ -1,32 +1,38 @@
+
 window.onload = function() {
 
 
 	console.log("Hello World");
 
 //Change the font color function
-document.getElementById('changeColorText').addEventListener ('click', function (event) {
-		document.body.style.color = "red";
-		//document.getElementById("changeColorText").src = "./js-assignment.js";
-	}
-);
+	document.getElementById('changeColorText').addEventListener('click', function(event) {
+			document.body.style.color = "red";
+			//document.getElementById("changeColorText").src = "./js-assignment.js";
+		}
+	);
 
 //Change the text font function
-	document.getElementById('changeTextFont').addEventListener ('click', function (event) {
+	document.getElementById('changeTextFont').addEventListener('click', function(event) {
 			document.body.style.fontFamily = "verdana";
 			//document.getElementById("changeTextFont").src = "./js-assignment.js";
 		}
 	);
 
-	//Shuffle words function
-	string.prototype.shuffle = function() {
-		var a = this.split(""),
-			n = a.length;
-
-		for(var i = n; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-		}
-	}
 };
 
+//Shuffling around words
+function shufflize() {
+	var txt = document.getElementById("shuffleWords").textContent;
+	var words = txt.split("");
+	words.map(function(t) {
+		for(i = (words.length - 1); i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = words[i];
+			words[i] = words[j];
+			words[j] = temp;
+		}
+		document.getElementById("shuffleWords").textContent = words.join(" ")
+	});
+}
 
 
